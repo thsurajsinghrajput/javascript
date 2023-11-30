f0ff0c2fb2b1ca9819765ae190c146554f2292f7
thsurajsinghrajput <thsurajsinghrajput@gmail.com>
2023-11-30 19:19:52 +0530

    inititial commit

---
+++ fundamental_part1/prob1.js
@@ -0,0 +1,7 @@
+let country = "india"
+let continent = "asia"
+let population = 1350;
+let laguage 
+let isIland = false
+console.log("name :-"+country +"\ncontinent :-" +continent+"\npopulation(in million):-"+population+"\nlaguage:-"+laguage+"\nis Iland?:-"+isIland)
+console.log()
---
+++ script.js
@@ -0,0 +1,24 @@
+ //////////////////////VARIABLE//////////////////
+//  cant use reserve keyword(exept name)
+// we use upper case for constant variable like let PI = 3.1415;
+//cant start with number or symbol execept _ 
+// let _a = 41;
+// let firstJob = "coder" // good way to name a variable
+
+
+
+//********************* */ datatype *************************
+// 1. Number: Floating point numbers � Used for decimals and integers
+// 2. String: Sequence of characters � Used for text
+// 3. Boolean: Logical type that can only be true or false � Used for taking decisions
+// 4. Undefined: Value taken by a variable that is not yet defined (‘empty value’)
+// 5. Null: Also means ‘empty value’
+// 6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+// 7. BigInt (ES2020): Larger integers than the Number type can hold
+// JavaScript has dynamic typing: We do not have to manually define the data type of
+// the value stored in a variable. Instead, data types are determined automatically.
+// Value has type, NOT variable!
+ 
+ 
+//  let d&r = 4;
+    console.log(3+3)
